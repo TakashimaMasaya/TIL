6ed69ff809d2a11e5a746c0acda20f47331ec653
TakashimaMasaya <53415292+TakashimaMasaya@users.noreply.github.com>
2021-03-16 00:31:54 +0800

    Add files via upload

---
+++ rubyPractice/FizzBuzz.rb
@@ -0,0 +1,21 @@
+# 1～任意の数までの数字を順番に表示する
+# ただし、3の倍数のときはFizz、5の倍数のときはBuzz、3と5の公倍数のときはFizzBuzzと表示する。
+# 任意の数はプログラム実行時に引数で渡せるようにする
+
+
+def fizzbuzz(x)
+  n = 0
+  while n < x
+    n = n + 1
+    if n % 3 == 0 && n % 5 == 0 then
+      p "FizzBuzz"
+    elsif n % 3 == 0 then
+      p "Fizz"
+    elsif n % 5 == 0 then
+      p "Buzz"
+    else
+      p n
+    end  
+  end
+end
+p fizzbuzz(ARGV[0].to_i)
